259dc1f20a3688c5e0d3f684f495d212d8ae028f
Bram Mak <acmbasketbal@gmail.com>
2015-10-12 22:54:19 +0200

    Added Motor Tasks + Compass supp.

--- Verdediger/libVerdediger.h
+++ Verdediger/libVerdediger.h
@@ -1,59 +1,101 @@
 //Motors
+
 /*
- * OUT_A:Fwd=rechts Rev=links
- * OUT_B:Fwd=achteruit Rev=vooruit
- * OUT_C:Fwd=rechtsom Rev=linksom
- * naar rechts: OnFwd(OUT_A,100); OnRev(OUT_C,55);
- * naar links: OnRev(OUT_A,100); OnFwd(OUT_C,58);
+ *OUT_A:Fwd=rechts Rev=links
+ *OUT_B:Fwd=achteruit Rev=vooruit
+ *OUT_C:Fwd=rechtsom Rev=linksom
+ *naar rechts: OnFwd(OUT_A, 100); OnFwd(OUT_C, -55);
+ *naar links: OnFwd(OUT_A, -100); OnFwd(OUT_C, 58);
  *
- *achteruit: OnFwd(OUT_B,100); OnRev(OUT_C,28);
+ *achteruit: OnFwd(OUT_B, 100); OnFwd(OUT_C, -28);
 */
+
 #define MOTORFORWARD OUT_B
 #define MOTORSIDE OUT_A
 #define COMPENSATOR OUT_C
 
+#define GoLeft() start LeftDriver
+#define GoRight() start RightDriver
+#define GoForward() start ForwardDriver
+#define GoBackward() start BackwardDriver
+
 task ForwardDriver()
 {
-
+    char correctingspeed;
+    stop LeftDriver;
+    stop RightDriver;
+    stop BackwardDriver;
+    Off(OUT_A);
+    OnFwd(OUT_B, -100);
+    OnFwd(OUT_C, 28);                                                           //Still needs to be calibrated
+    while(true)
+    {
+        correctingspeed = 28 - RELCOMPASSVAL / 4;                               //Dit dan ook nog goed instellen
+        OnFwd(OUT_C, correctingspeed);
+    }
 }
 
 task BackwardDriver()
 {
-
+    char correctingspeed;
+    stop LeftDriver;
+    stop RightDriver;
+    stop ForwardDriver;
+    Off(OUT_A);
+    OnFwd(OUT_B, 100);
+    OnFwd(OUT_C, -28);
+    while(true)
+    {
+        correctingspeed = -28 - RELCOMPASSVAL / 4;
+        OnFwd(OUT_C, correctingspeed);
+    }
 }
 
 task LeftDriver()
 {
-
+    char correctingspeed;
+    stop RightDriver;
+    stop ForwardDriver;
+    stop BackwardDriver;
+    Off(OUT_B);
+    OnFwd(OUT_A, -100);
+    OnFwd(OUT_C, 58);
+    while(true)
+    {
+        correctingspeed = 58 - RELCOMPASSVAL / 4;
+        OnFwd(OUT_C, correctingspeed);
+    }
 }
 
 task RightDriver()
 {
-
+    char correctingspeed;
+    stop LeftDriver;
+    stop ForwardDriver;
+    stop BackwardDriver;
+    Off(OUT_B);
+    OnFwd(OUT_A, 100);
+    OnFwd(OUT_C, -55);
+    while(true)
+    {
+        correctingspeed = -55 - RELCOMPASSVAL / 4;
+        OnFwd(OUT_C, correctingspeed);
+    }
 }
 
 //Port aliases
 #define IRSEEKERPORT S1
-#define LIGHTSENSORPORT S2
+#define COMPASSSENSORPORT S2
 #define USSENSORLEFTPORT S3
 #define USSENSORBACKPORT S4
 
 //Sensor value aliases
-#define UpdateIRValues() HTEnhancedIRSeekerV2(IRSEEKERPORT, richting, afstand)
-#define LIGHTVAL SENSOR_2
+#define UpdateIRValues() HTEnhancedIRSeekerV2(IRSEEKERPORT, dir, dist)
+#define RAWCOMPASSVAL SensorHTCompass(S2)
+#define COMPASSVAL CompassVal()
+#define RELCOMPASSVAL RelCompassVal()
 #define USLEFTVAL SensorUS(USSENSORLEFTPORT)
 #define USBACKVAL SensorUS(USSENSORBACKPORT)
-
-//IRBall
-#define BALLDIRLEFT (richting > 5)
-#define BALLDIRRIGHT (richting < 5)
-#define BALLDIRSTRAIGHT (richting == 5)
-#define BALLDIRUNKNOWN (richting == 0)
-#define BALLDISTANCETHRESHOLD 300
-
-int richting;
-int afstand;
-char lastballstate;
 
 //Position
 #define XPOS (USLEFTVAL - x0)
@@ -62,7 +104,18 @@ char lastballstate;
 unsigned char x0;
 unsigned char y0;
 
-void HTEnhancedIRSeekerV2(const byte  port, int &dir = richting, int &strength = afstand)
+//IRBall
+#define BALLDIRLEFT (dir > 5)
+#define BALLDIRRIGHT (dir < 5)
+#define BALLDIRSTRAIGHT (dir == 5)
+#define BALLDIRUNKNOWN (dir == 0)
+#define BALLDISTANCETHRESHOLD 300
+
+int dir;
+int dist;
+char lastballstate;
+
+void HTEnhancedIRSeekerV2(const byte  port, int &dir = dir, int &strength = dist)
 {
   int cResp;
   byte cmdBuf[] = {0x10, 0x43};
@@ -103,22 +156,55 @@ void HTEnhancedIRSeekerV2(const byte  port, int &dir = richting, int &strength =
   }
 }
 
+//Compass
+short compassbeginval;                                                          //compassbeginval =  COMPASSVAL;
+
+short CompassVal()
+{
+    if(RAWCOMPASSVAL < 0)
+    {
+        return RAWCOMPASSVAL - compassbeginval + 360;
+    }
+    else
+    {
+        return RAWCOMPASSVAL - compassbeginval;
+    }
+}
+
+short RelCompassVal()
+{
+    short tmp = RAWCOMPASSVAL - compassbeginval;
+    if(tmp <= 180 && tmp >= -179)
+    {
+        return tmp;
+    }
+    if(tmp > 180)
+    {
+        return tmp - 360;
+    }
+    if(tmp <= -180)
+    {
+        return tmp + 360;
+    }
+}
+
+//Display
 void DrawSensorLabels()
 {
     TextOut(0,  LCD_LINE1, "IRdir:");
     TextOut(0,  LCD_LINE2, "IRdist:");
-    TextOut(0,  LCD_LINE3, "Light:");
-    TextOut(0,  LCD_LINE4, "US1:");
-    TextOut(50, LCD_LINE4, "US2:");
+    TextOut(0,  LCD_LINE3, "Compass:");
+    TextOut(0,  LCD_LINE4, "USL:");
+    TextOut(50, LCD_LINE4, "USB:");
 }
 
 void DrawSensorValues()
 {
-    NumOut(50,  LCD_LINE1, richting);
+    NumOut(50,  LCD_LINE1, dir);
     TextOut(50, LCD_LINE2, "    ");
-    NumOut(50,  LCD_LINE2, afstand);
+    NumOut(50,  LCD_LINE2, dist);
     TextOut(50, LCD_LINE3, "   ");
-    NumOut(50,  LCD_LINE3, LIGHTVAL);
+    NumOut(50,  LCD_LINE3, RELCOMPASSVAL);
     TextOut(25, LCD_LINE4, "   ");
     NumOut(25,  LCD_LINE4, USLEFTVAL);
     TextOut(75, LCD_LINE4, "   ");
